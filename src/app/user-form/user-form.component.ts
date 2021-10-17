e9652a59b01b69c65fc0a4576da6eb0429d14f58
emelda <YOUR EMAIL ADDRESS>
2021-10-17 15:21:48 +0300

    create a class.

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-form',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent implements OnInit {
+  searchUser!:string
+  @Output() searchUsername= new EventEmitter<any>();
+
+  search(){
+    this.searchUsername.emit(this.searchUser)
+    this.searchUser=''
+  }
+
 
   constructor() { }
 
