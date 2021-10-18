3a17bcacfce17e3c8d2051a3191b540a73c4128a
emelda <YOUR EMAIL ADDRESS>
2021-10-18 13:16:08 +0300

    style the homepage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { RepComponent } from './rep/rep.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path:'home', component: HomeComponent},
   { path:'profiles', component: ProfileComponent},
   { path: 'repo',component:RepComponent}
+
 ];
 
 @NgModule({
